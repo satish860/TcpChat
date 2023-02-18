11ca432d8a7e7f6aad2fa67983b9f06063a31134
Satish Venkatakrishnan <satish860@gmail.com>
2023-02-18 10:05:09 +0530

    TCP Chat Application Initial Check in

---
+++ Chat/Chat.Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Chat.Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Press Enter to Start the Client connection with the server.");
+
+            Console.ReadKey();
+
+            var _clientConnection = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            await _clientConnection.ConnectAsync("localhost", 33333);
+
+            Console.WriteLine($"Connected to server at {_clientConnection.RemoteEndPoint}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chat/Chat.Server/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Chat.Server
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listenSocket.Bind(new IPEndPoint(IPAddress.Any, 33333));
+            listenSocket.Listen();
+            Console.WriteLine("Starting Server...");
+            while (true)
+            {
+                var connectedSocket = await listenSocket.AcceptAsync();
+                Console.WriteLine($"Connection accepted from {connectedSocket.RemoteEndPoint} on {connectedSocket.LocalEndPoint}");
+            }
+
+
+        }
+    }
+}
