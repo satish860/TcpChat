3b67ee8e14ceb205723c93601966b5960cbb3af8
Satish Venkatakrishnan <satish860@gmail.com>
2023-02-20 11:57:07 +0530

    We are sending the Data for server

--- Chat/Chat.Client/Program.cs
+++ Chat/Chat.Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.IO.Pipelines;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -10,15 +11,48 @@ namespace Chat.Client
         {
             Console.WriteLine("Press Enter to Start the Client connection with the server.");
 
-            Console.ReadKey();
-
-            var _clientConnection = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            var _clientConnection = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             await _clientConnection.ConnectAsync("localhost", 33333);
+
+            var pipe = new Pipe();
+            _ = PipeLineToSocketAsync(pipe.Reader, _clientConnection);
+
 
             Console.WriteLine($"Connected to server at {_clientConnection.RemoteEndPoint}");
             var message = Encoding.UTF8.GetBytes("Hello World!!");
-            await _clientConnection.SendAsync(message, SocketFlags.None);
+            var memory = pipe.Writer.GetMemory(message.Length);
+            message.CopyTo(memory);
+            pipe.Writer.Advance(message.Length);
+            await pipe.Writer.FlushAsync();
             Console.ReadKey();
+        }
+
+
+        public static async Task PipeLineToSocketAsync(PipeReader pipeReader, Socket socket)
+        {
+            while (true)
+            {
+                ReadResult readResult = await pipeReader.ReadAsync();
+                var buffer = readResult.Buffer;
+
+                while (true)
+                {
+                    var memory = buffer.First;
+                    if (memory.IsEmpty)
+                        break;
+                    var bytesSent = await socket.SendAsync(memory, SocketFlags.None);
+                    buffer = buffer.Slice(bytesSent);
+                    if (bytesSent!= memory.Length)
+                    {
+                        break;
+                    }
+                }
+                pipeReader.AdvanceTo(buffer.Start);
+                if (readResult.IsCompleted)
+                {
+                    break;
+                }
+            }
         }
     }
 }
